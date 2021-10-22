de5e328c2ba782d25f79dee24988b8f6a8109dda
RoyalVee <victoroguche@gmail.com>
2021-10-22 20:15:40 +0100

    update to sql script

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -24,6 +24,37 @@ ORDER BY 1,2;
 -- Looking at the percantage of population that got COVID
 SELECT location, date, total_cases, population, (total_cases/population)*100 percentage_of_population_withCOVID
 FROM [Portfolio Projects]..CovidDeath
-WHERE location LIKE '%india%'
+--WHERE location LIKE '%india%'
 ORDER BY 1,2;
+
+
+-- Looking out for the highest cases by countries
+SELECT location, population, MAX(CAST(total_cases as int)) maxNumberOfCases
+FROM [Portfolio Projects]..CovidDeath
+WHERE location NOT IN ('Africa','Asia','Europe','North America','South America', 'Australia', 'Antarctica', 'World')
+GROUP BY location, population
+ORDER BY 3 DESC;
+
+
+-- Looking out for the highest cases by countries relative to their total population
+SELECT location, population, MAX(CAST(total_cases as int)) maxNumberOfCases, MAX((total_cases/population)*100) highest_casesByPopulation
+FROM [Portfolio Projects]..CovidDeath
+WHERE location NOT IN ('Africa','Asia','Europe','North America','South America', 'Australia', 'Antarctica', 'World')
+GROUP BY location, population
+ORDER BY 4 DESC;
+
+-- Looking out for the highest death by countries
+SELECT location, population, MAX(CAST(total_deaths as int)) maxNumberOfDeath
+FROM [Portfolio Projects]..CovidDeath
+WHERE location NOT IN ('Africa','Asia','Europe','North America','South America', 'Australia', 'Antarctica', 'World')
+GROUP BY location, population
+ORDER BY 3 DESC;
+
+
+-- Looking out for the highest death by countries relative to their population
+SELECT location, population, MAX(CAST(total_deaths as int)) maxNumberOfDeath, MAX((total_deaths/population)*100) percentageOfPopulationDead
+FROM [Portfolio Projects]..CovidDeath
+WHERE location NOT IN ('Africa','Asia','Europe','North America','South America', 'Australia', 'Antarctica', 'World')
+GROUP BY location, population
+ORDER BY 4 DESC;
 
